ca849b911d8d82af06b713bf63d1ad4d5107f9e0
Lucky Shrestha <luckystha92@gmail..com>
2025-04-19 15:01:22 +0545

    fix: CORS issue for vercel frontend

--- server/server.js
+++ server/server.js
@@ -3,26 +3,35 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from './config/mongodb.js';
-import authRouter from './routes/authRoutes.js'
+import authRouter from './routes/authRoutes.js';
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 connectDB();
 
 const allowedOrigins = [
-    'http://localhost:5173', // local dev
-    'https://mern-authentication-kappa.vercel.app',
-  ];
+  "http://localhost:5173",
+  "https://mern-authentication-kappa.vercel.app",
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}))
 
+// API Endpoints
+app.get("/", (req, res) => res.send("API Working"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-//API Endpoints
-app.get('/', (req,res)=> res.send("API Working"));
-app.use('/api/auth', authRouter)
-app.use('/api/user', userRouter)
-
-app.listen(port, ()=> console.log(`Server started on PORT:${port}`));
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
